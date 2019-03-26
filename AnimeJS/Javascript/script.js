5bba7cf359252861c1e6d72a7699f914842519a6
Jimmy Svensson <jimmy888swe@gmail.com>
2019-03-26 15:19:13 +0100

    done

--- AnimeJS/Javascript/script.js
+++ AnimeJS/Javascript/script.js
@@ -2,21 +2,24 @@ let conterinerWidth = document.querySelector('.container').offsetWidth;
 let numOfImg = document.querySelector('.slider').childElementCount;
 let activeSlide = 0;
 let animationTime = 500;
+
+let animation = () => {
+        anime({
+        targets: '.slider',
+        duration: animationTime,
+        translateX: -(activeSlide * conterinerWidth),
+        easing: 'easeInOutQuad'
+        });
+    };
 document.querySelector('.slider').style.width = `${conterinerWidth*numOfImg}px`;
 
 document.querySelector('#prev').addEventListener('click', () => {
     // <-
     if (activeSlide > 0) {
-               // föregående bild
-               activeSlide--
-
-               //animering med animeJS
-               anime({
-                   targets: '.slider',
-                   duration: animationTime,
-                   translateX: -(activeSlide * conterinerWidth),
-                   easing: 'easeInOutQuad'
-               })
+        // föregående bild
+        activeSlide--
+        //animering med animeJS
+        animation();
     }
 })
 document.querySelector('#next').addEventListener('click', () => {
@@ -25,18 +28,7 @@ document.querySelector('#next').addEventListener('click', () => {
     if (activeSlide < numOfImg - 1) {
         // näste bild
         activeSlide++
-
         //animering med animeJS
-        anime({
-            targets: '.slider',
-            duration: animationTime,
-            translateX: -(activeSlide * conterinerWidth),
-            easing: 'easeInOutQuad'
-        })
-
-    } else {
-        
-        //gör nada
-    }
-
+        animation();
+    } 
 })
